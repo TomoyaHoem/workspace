4dced406ce3576c86b88e8a8fe13646b6063cdda
Tomoya-H <tomoya75@hotmail.de>
2023-04-06 16:57:21 +0900

    finished single-objective implementation - need to fix fitness function - cleanup and documentate

--- single-objective-ga.py
+++ single-objective-ga.py
@@ -15,12 +15,36 @@ print("-" * os.get_terminal_size().columns)
 print(f"Single-Objective Optimization using DEAP-framework")
 print("-" * os.get_terminal_size().columns)
 
+# target to be found
 target_sequence = "To be, or not to be."
+target_len = len(target_sequence)
 
 print("Solve Shakespearean Monkey Problem using EA:")
 print()
-print(f"Target sequence is: {target_sequence}, with length: {len(target_sequence)}")
+print(f"Target sequence is: {target_sequence}, with length: {target_len}")
 print("\n")
+
+# char alphabet (lower and uppercase ascii as well as whitespaces)
+alphabet = string.ascii_letters + string.whitespace
+
+
+# evaluation function (#of incorrect chars / target_len)
+def evalFitness(individual: list) -> float:
+    correct_count = 0
+    for i in range(target_len):
+        if individual[i] == target_sequence[i]:
+            correct_count += 1
+
+    return correct_count / target_len
+
+
+# mutation function (randomize some chars depending on prob)
+def mutRandChar(individual: list, indpb: float) -> list:
+    for i in range(target_len):
+        if random.random() < indpb:
+            individual[i] = random.choice(alphabet)
+
+    return individual
 
 
 def main() -> None:
@@ -38,7 +62,7 @@ def main() -> None:
     toolbox = base.Toolbox()
     ## here we create individuals as random words -> random char list
     ## first define way to generate random chars
-    toolbox.register("attr_char", random.choice, string.ascii_letters)
+    toolbox.register("attr_char", random.choice, alphabet)
     ## then, individuals from repeating these chars
     toolbox.register(
         "individual",
@@ -50,13 +74,69 @@ def main() -> None:
     ## and finally a population of individuals
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    # evaluation function (#of incorrect chars / target_len)
-
     # genetic operators
+    toolbox.register("evaluate", evalFitness)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", mutRandChar, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    # evolution setup
+
+    ## create population
+    pop = toolbox.population(n=100)
+    ## evaluate entire population
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    ## constants
+    CXPB, MUTPB = 0.5, 0.2
+
+    fits = [ind.fitness.values[0] for ind in pop]
 
     # evolution loop
-    ind = toolbox.individual()
-    print(ind)
+    g = 0
+
+    while max(fits) < 1 and g < 1000:
+        g += 1
+        print(f"-- Generation {g} --")
+
+        # select next generation of individuals
+        offspring = toolbox.select(pop, len(pop))
+        # clone selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+
+        # apply crossover and mutation on offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        # evaluate individuals with invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        pop[:] = offspring
+
+        # gather all fitnesses in one list and print stats
+        fits = [ind.fitness.values[0] for ind in pop]
+
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean**2) ** 0.5
+
+        print(f"  Min {min(fits)}")
+        print(f"  Max {max(fits)}")
+        print(f"  Avg {mean}")
+        print(f"  Std {std}")
 
     print("Finished EA")
 
