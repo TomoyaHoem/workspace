2eb6895b07044cd1402381e54b60ac073f52da2b
Tomoya-H <tomoya75@hotmail.de>
2023-05-30 14:28:50 +0900

    Split reading into limited and all

--- pickle-zinc.py
+++ pickle-zinc.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import PandasTools
+from alive_progress import alive_bar
+
+TOTAL_NUM_MOLS = 1300000
 
 
 def SetDTypes(df):
@@ -11,22 +14,43 @@ def SetDTypes(df):
     return df
 
 
-def ReadMols(cwd, maxFromFile, molecules) -> None:
-    index = 0
+def ReadAllMols(cwd, molecules) -> None:
+    count = 0
+
+    with alive_bar(TOTAL_NUM_MOLS) as bar:
+        for root, dirs, files in os.walk(cwd):
+            for file in files:
+                curP = root + "\\" + file
+                suppl = Chem.SmilesMolSupplier(curP)
+                for mol in suppl:
+                    curMol = [root[-2:], file, mol, Chem.MolToSmiles(mol)]
+                    molecules.loc[count] = curMol
+                    count += 1
+                    bar()
+
+    print(f"Stored a total of {count} molecules")
+
+
+def ReadMolsWithLimit(cwd, maxFromFile, molecules) -> None:
+    count = 0
 
     for root, dirs, files in os.walk(cwd):
         for file in files:
+            index = 0
             # print(f"Storing {file}")
             curP = root + "\\" + file
             suppl = Chem.SmilesMolSupplier(curP)
             for mol in suppl:
-                if index != 0 and index % maxFromFile == 0:
+                if index > maxFromFile:
                     # print("LIMIT")
                     break
                 curMol = [root[-2:], file, mol, Chem.MolToSmiles(mol)]
                 # print(f"SMILES no {index} + {curMol}")
-                molecules.loc[index] = curMol
+                molecules.loc[count] = curMol
+                count += 1
                 index += 1
+
+    print(f"Stored a total of {count} molecules")
 
 
 def main() -> None:
@@ -39,12 +63,12 @@ def main() -> None:
     cwd = os.getcwd() + "\ZINCSMILES"
     # current index to store in dataframe
     maxFromFile = 1000
-    ReadMols(cwd, maxFromFile, molecules)
+    ReadMolsWithLimit(cwd, maxFromFile, molecules)
 
     # downcast directory and filename to category to save memory
-    print(molecules.info())
-    molecules = SetDTypes(molecules)
-    print(molecules.info())
+    # print(molecules.info())
+    # molecules = SetDTypes(molecules)
+    # print(molecules.info())
 
     # add molecule column to dataframe, does not work currently?
     # PandasTools.AddMoleculeColumnToFrame(molecules, "Smiles", "Molecule")
