4c289fbe61a542d89e855256f1e13ac1a6a14293
Tomoya-H <tomoya75@hotmail.de>
2023-05-30 14:09:15 +0900

    separate class for reading and pickling zinc data

--- pareto.py
+++ pareto.py
@@ -9,22 +9,6 @@ def main() -> None:
     # read all smiles strings
     cwd = os.getcwd() + "\ZINCSMILES"
 
-    molecules = []
-
-    for dirnum, (root, dirs, files) in enumerate(os.walk(cwd)):
-        for dir in dirs:
-            molecules.append([])
-
-        for filenum, file in enumerate(files):
-            if dirnum > 1:
-                break
-            molecules[dirnum - 1].append([])
-            curP = root + "\\" + file
-            suppl = Chem.SmilesMolSupplier(curP)
-            for molnum, mol in enumerate(suppl):
-                print(f"Storing SMILES: {molnum} of {curP}")
-                molecules[dirnum - 1][filenum - 1].append(mol)
-
     # evaluate all molecules for logp, sa, qed
 
     # plot result
