39c4e27e7b4f63219bd596adad48bd76fa7a900e
Tomoya-H <tomoya75@hotmail.de>
2023-05-30 14:58:24 +0900

    downloaded sascorer

--- pareto.py
+++ pareto.py
@@ -1,17 +1,38 @@
 from rdkit import Chem
+from rdkit.Chem import QED
+from rdkit.Chem import Crippen
 import os
 import numpy as np
 import pandas as pd
+import time
 
 
 def main() -> None:
     print(f"Entry")
 
+    start = time.time()
     # unpickle
-    molecules = pd.read_pickle("./pkl/shards.pkl")
+    molecules = pd.read_pickle("./pkl/100-shards.pkl")
     print(molecules.head())
+    end = time.time()
+    dur = round(end - start, 3)
 
-    # evaluate all molecules for logp, sa, qed
+    print(f"Elapsed time to unpickle: {dur}s")
+
+    # evaluate all molecules for qed, log, sa
+    start = time.time()
+    molecules["QED"] = molecules["Mol"].apply(QED.default)
+    print(molecules.head())
+    end = time.time()
+    dur = round(end - start, 3)
+    print(f"Elapsed time to calculate QED: {dur}s")
+
+    start = time.time()
+    molecules["LogP"] = molecules["Mol"].apply(Crippen.MolLogP)
+    print(molecules.head())
+    end = time.time()
+    dur = round(end - start, 3)
+    print(f"Elapsed time to calculate LogP: {dur}s")
 
     # plot result
 
