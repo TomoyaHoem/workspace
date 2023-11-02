7b3a6acee91800c748d2e15560c71bc3ae0e5941
Tomoya-H <tomoya75@hotmail.de>
2023-11-02 12:19:24 +0900

    faster read

--- subset-pickle.py
+++ subset-pickle.py
@@ -3,7 +3,14 @@ import pandas as pd
 from alive_progress import alive_bar
 
 TOTAL_NUM_MOLS = 435_000_000
-MOL_PERCENTAGE_PER_FILE = 0.001
+MOL_PERCENTAGE_PER_FILE = 0.0001
+
+def blocks(files, size=65536):
+    while True:
+        b = files.read(size)
+        if not b:
+            break
+        yield b
 
 
 def read_molecules(cwd: str) -> pd.DataFrame:
@@ -14,17 +21,19 @@ def read_molecules(cwd: str) -> pd.DataFrame:
             for file in files:
                 index = 0
                 curP = os.path.join(root, file)
-                num_lines = sum(1 for _ in open(curP))
+                with open(curP, "r", encoding="utf-8", errors="ignore") as f:
+                    num_lines = sum(bl.count("\n") for bl in blocks(f))
+                num_line = sum(1 for _ in open(curP))
                 for line in open(curP):
                     if index > int(MOL_PERCENTAGE_PER_FILE * num_lines):
                         # print("LIMIT")
                         break
-                    smile = line
-                    mol_row = {
-                        "Smiles": smile,
-                    }
-                    # print(f"SMILES no {index} + {curMol}")
-                    dictionary_list.append(mol_row)
+                    # smile = line
+                    # mol_row = {
+                    #     "Smiles": smile,
+                    # }
+                    # # print(f"SMILES no {index} + {curMol}")
+                    # dictionary_list.append(mol_row)
                     index += 1
                     bar()
 
@@ -47,7 +56,7 @@ def main() -> None:
 
     # pickle dataframe
     print("pickle...")
-    molecules.to_pickle("./pkl/subset.pkl")
+    #molecules.to_pickle("./pkl/subset.pkl")
     print("--- Finished Pickling ---")
 
 
