0017a35edf231aa5c51e1cb8adc5e9d30ff99f4c
Tomoya-H <tomoya75@hotmail.de>
2023-11-16 11:04:07 +0900

    check if distributions match

--- distribution.py
+++ distribution.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
+import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def concatPkl() -> None:
     # read pkl files and concatenate to one dataframe
@@ -19,11 +20,19 @@ def concatPkl() -> None:
 def main() -> None:
     print("Entry")
 
-    concatPkl()
+    # concatPkl()
+    molecules = pd.read_pickle("./pkl/subset-indicators.pkl")
+    print(molecules.info())
+    print(molecules.head())
 
     # plot histograms of columns
+    hist = molecules.hist()
+    plt.show(block=False)
 
     # compare to sampled hitograms
+    mol_sample = molecules.sample(100)
+    hist_s = mol_sample.hist()
+    plt.show()
 
 
 if __name__ == "__main__":
