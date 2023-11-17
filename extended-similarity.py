f12c6b18be6abfe1a9a121dad670ab4ee1389f1b
Tomoya-H <tomoya75@hotmail.de>
2023-11-17 13:42:54 +0900

    similarity stress

--- extended-similarity.py
+++ extended-similarity.py
@@ -1,10 +1,13 @@
+import time
 import numpy as np
+import pandas as pd
 from indices.baroni_urbani_buser import BaroniUrbaniBuser
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
 
 def fingerprints(molecules):
+    start =  time.time()
     mol_fps = []
     for molecule in molecules:
         mol_fps.append(
@@ -14,6 +17,10 @@ def fingerprints(molecules):
                 )
             )
         )
+    end = time.time()
+    dur = round(end - start, 3)
+
+    print(f"Elapsed time to calculate fingerprints: {dur}s")
     return np.array(mol_fps)
 
 
@@ -22,28 +29,33 @@ def internal_similarity(molecules, c_threshold=None, w_factor="fraction") -> flo
     Returns the extended BuB similarity for the list of molecules
     """
     fps = fingerprints(molecules)
+    start =  time.time()
     multiple_comparisons_index = BaroniUrbaniBuser(
         fingerprints=fps, c_threshold=c_threshold, w_factor=w_factor
     )
-    return multiple_comparisons_index.__dict__["BUB_1sim_wdis"]
+    end = time.time()
+    dur = round(end - start, 3)
+
+    print(f"Elapsed time to calculate similarity: {dur}s")
+    return multiple_comparisons_index.__dict__["BUB_1sim_dis"]
 
 
 def main() -> None:
-    a = [
-        "OCCc1c(C)[n+](cs1)Cc2cnc(C)nc2N",
-        "CC(C)[C@@]12C[C@@H]1[C@@H](C)C(=O)C2",
-        "CC[C@H](O1)CC[C@@]12CCCO2",
-        "CC(=O)OCCC(/C)=C\C[C@H](C(C)=C)CCC=C",
-        "O1C=C[C@H]([C@H]1O2)c3c2cc(OC)c4c3OC(=O)C5=C4CCC(=O)5",
-        "CCC[C@@H](O)CC/C=C/C=C/C#CC#C/C=C/CO",
-        "CN1CCC[C@H]1c2cccnc2",
-        "CCc(c1)ccc2[n+]1ccc3c2[nH]c4c3cccc4",
-        "O=Cc1ccc(O)c(OC)c1",
-        "O=Cc1ccc(O)c(OC)c1",
-        "COc1cc(C=O)ccc1O",
-    ]
+    molecules = pd.read_pickle("./pkl/subset-indicators.pkl")
+    print(molecules.info())
+    print(molecules.head())
 
-    print(internal_similarity(a))
+    num = 500_000
+    sample = molecules.sample(num)
+
+    mol_list = sample['Smiles'].tolist()
+
+    start =  time.time()
+    print(internal_similarity(mol_list))
+    end = time.time()
+    dur = round(end - start, 3)
+
+    print(f"Elapsed time to calculate fingerprints and similarity for {num} molecules: {dur}s")
 
 
 if __name__ == "__main__":
